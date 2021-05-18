10d13b8f89f0075dd77902822cb4e8d1900f1a39
Tuomas Puikkonen <tuomas.puikkonen@iki.fi>
2021-05-18 13:38:24 +0300

    Testing vscode git commands

--- osa04-02_viiva/src/viiva.py
+++ osa04-02_viiva/src/viiva.py
@@ -1,4 +1,12 @@
 # tee ratkaisu tänne
 # funktiota kannattaa testata kutsumalla sitä täällä seuraavasti
+
+def viiva(pituus, mjono):
+    if len(mjono) == 0:
+        print("*"*pituus)
+    else:
+        print(mjono[0]*pituus)
+
 if __name__ == "__main__":
     viiva(5, "x")
+    viiva(5, "")
