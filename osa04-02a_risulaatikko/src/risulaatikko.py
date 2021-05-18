29d49567e4bbee602186c628a6de6811c2f81518
Tuomas Puikkonen <tuomas.puikkonen@iki.fi>
2021-05-18 14:12:45 +0300

    Testing 2

--- osa04-02a_risulaatikko/src/risulaatikko.py
+++ osa04-02a_risulaatikko/src/risulaatikko.py
@@ -1,9 +1,19 @@
 # kopioi edellisestä tehtävästä funktion viiva koodi tänne
 
+def viiva(pituus, mjono):
+    if len(mjono) == 0:
+        print("*"*pituus)
+    else:
+        print(mjono[0]*pituus)
+
 def risulaatikko(korkeus):
     # täällä tulee kutsua funktiota viiva sopivilla parametreilla
-    viiva(10, "#")
+    apuluku = 0
+    while apuluku < korkeus:
+        viiva(10, "#")
+        apuluku += 1
 
 # funktiota kannattaa testata kutsumalla sitä täällä seuraavasti
 if __name__ == "__main__":
     risulaatikko(5)
+    print("testi")
