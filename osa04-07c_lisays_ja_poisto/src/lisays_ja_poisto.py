63a0a0cb79db9a3b3cf8d1c37d8149246f13ac46
Tuomas Puikkonen <tuomas.puikkonen@iki.fi>
2021-05-29 22:14:43 +0300

    solved some excercises

--- osa04-07c_lisays_ja_poisto/src/lisays_ja_poisto.py
+++ osa04-07c_lisays_ja_poisto/src/lisays_ja_poisto.py
@@ -1,1 +1,20 @@
 # Kirjoita ratkaisu tähän
+
+lista = []
+apuluku = 0
+print(f"Lista on nyt {lista}")
+
+while True:
+    mjono = input("(l)isää, (p)oista vai e(x)it: ")
+    if mjono == "x":
+        print("Moi!")
+        break
+    elif mjono == "l":
+        apuluku += 1
+        lista.append(apuluku)
+        print(f"Lista on nyt {lista}")
+    elif mjono == "p":
+        apuluku -= 1
+        lista.pop(len(lista)-1)
+        print(f"Lista on nyt {lista}")
+
